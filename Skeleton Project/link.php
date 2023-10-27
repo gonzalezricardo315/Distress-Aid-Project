a0cb3d9c6fe7680e556961b1e887dd96d1807103
gonzalezricardo315 <Gonzalezricardo315@yahoo.com>
2023-10-27 11:07:45 -0600

    changed some things in the apply page and added the backend for the apply page with php

---
+++ Skeleton Project/link.php
@@ -0,0 +1,27 @@
+<?php 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $dob = $_POST['dob'];
+    $email = $_POST['email'];
+    $phone_number = $_POST['phone_number'];
+    $address = $_POST['address'];
+    $assistance_needed = $_POST['assistance_needed'];
+    $primary_name = $_POST['primary_name'];
+    $primary_number = $_POST['primary_number'];
+    $secondary_name = $_POST['secondary_name'];
+    $secondary_number = $_POST['secondary_number'];
+
+
+    // database connection
+    $conn = new mysqli('localhost','root','','application');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("insert into apply(first_name, last_name, dob, email, phone_number, address, assistance_needed, primary_name, primary_number, secondary_name, secondary_number) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssssssss", $first_name, $last_name, $dob, $email, $phone_number, $address, $assistance_needed, $primary_name, $primary_number, $secondary_name, $secondary_number);
+        $stmt->execute();
+        echo "registration Successfully...";
+        $stmt->close();
+        $conn->close();
+    }
+?>
