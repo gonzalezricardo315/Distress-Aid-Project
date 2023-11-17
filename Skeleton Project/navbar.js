9d9dfc0b6ce4ce32d842c9cb7466588872ef8bc2
cham24 <cameronchambers1215@gmail.com>
2023-11-16 19:36:29 -0700

    updated navbar + footer elements

--- Skeleton Project/navbar.js
+++ Skeleton Project/navbar.js
@@ -1,8 +1,25 @@
-function navbar() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+/* JS Functions for navbar */
+let menuIcon = document.querySelector('.menuIcon');
+let nav = document.querySelector('.overlay-menu');
+
+menuIcon.addEventListener('click', () => {
+    if (nav.style.transform != 'translateX(0%)') {
+        nav.style.transform = 'translateX(0%)';
+        nav.style.transition = 'transform 0.2s ease-out';
     } else {
-        x.style.display = "block";
+        nav.style.transform = 'translateX(-100%)';
+        nav.style.transition = 'transform 0.2s ease-out';
     }
-}
+});
+
+
+// Toggle Menu Icon ========================================
+let toggleIcon = document.querySelector('.menuIcon');
+
+toggleIcon.addEventListener('click', () => {
+    if (toggleIcon.className != 'menuIcon toggle') {
+        toggleIcon.className += ' toggle';
+    } else {
+        toggleIcon.className = 'menuIcon';
+    }
+});
